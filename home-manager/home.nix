4981bded9d37b05753c36e60b077d45d8129caad
Alexandre Ros <alexland7219@gmail.com>
2025-11-22 15:43:32 +0100

    Gen #78-T on Nov. 22, 2025

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -6,9 +6,6 @@
 }:
 
 {
-  home.username = "alex";
-  home.homeDirectory = "/home/alex";
-
   home.stateVersion = "24.05";
 
   home.sessionVariables.EDITOR = "vim";
@@ -19,7 +16,6 @@
   };
 
   # Packages
-
   home.packages = with pkgs; [
     tree
     git
@@ -32,7 +28,6 @@
     erlang
     qbittorrent
     dolphin-emu
-    vscodium
     vlc
     keepassxc
     texmaker
@@ -110,7 +105,69 @@
     })
   ];
 
-  programs.jujutsu.enable = true;
+  programs.jujutsu = {
+    enable = true;
+    settings = {
+      user.email = "alexland7219@gmail.com";
+      user.name = "Alexandre Ros";
+      ui.paginate = "never";
+      colors = {
+        author = "yellow";
+        change_id = "#EC0868";
+        "working_copy author" = "yellow";
+        "working_copy change_id" = "#EC0868";
+      };
+      template-aliases = {
+        "format_short_signature(signature)" = "signature.email().local()";
+      };
+    };
+  };
+
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+
+    profiles.default.userSettings = {
+      "files.autoSave" = "afterDelay";
+      "[nix]"."editor.tabSize" = 2;
+      "workbench.iconTheme" = "vscode-icons";
+      "workbench.colorTheme" = "GitHub Dark";
+      "editor.cursorBlinking" = "smooth";
+      "editor.fontFamily" = "'JetBrains Mono'";
+      "editor.formatOnPaste" = true;
+      "terminal.integrated.stickyScroll.enabled" = false;
+    };
+
+    profiles.default.extensions =
+      with pkgs.vscode-extensions;
+      [
+        vscodevim.vim
+        vscode-icons-team.vscode-icons
+        github.github-vscode-theme
+        enkia.tokyo-night
+        teabyii.ayu
+      ]
+      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "uiua-vscode";
+          publisher = "uiua-lang";
+          version = "0.0.67";
+          sha256 = "Q/wJZ+ObCU+hRpZZKQGQtdt99/I6QHkSuHlNy7oe5Pk=";
+        }
+        {
+          name = "bqn";
+          publisher = "mk12";
+          version = "0.2.0";
+          sha256 = "nTnL75BzHrpnJVO8DFfrLZZGavCC4OzvAlyrGCXSak4=";
+        }
+        {
+          name = "nix-ide";
+          publisher = "jnoortheen";
+          version = "0.5.0";
+          sha256 = "jVuGQzMspbMojYq+af5fmuiaS3l3moG8L8Kyf40vots=";
+        }
+      ];
+  };
 
   programs.thunderbird = {
     enable = true;
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -24,6 +24,7 @@
   ];
 
   home-manager = {
+    backupFileExtension = "bak";
     useGlobalPkgs = true;
     extraSpecialArgs = { inherit unstablePkgs; };
     users = {
